d570630c0ae24224592323581d11909d50e8cf21
nickrak <nickrak.nr@gmail.com>
2011-09-17 15:34:14 -0700

    Layout and plugin.yml

---
+++ src/nickrak/pluginbuilder/PluginBuilder.java
@@ -0,0 +1,26 @@
+package nickrak.pluginbuilder;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginBuilder extends JavaPlugin
+{
+	@Override
+	public void onDisable()
+	{
+
+	}
+
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		return super.onCommand(sender, command, label, args);
+	}
+}
